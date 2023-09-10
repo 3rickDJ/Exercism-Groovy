554591ba99e835011c66c8caf3f0a4486475358f
3rickDJ <erickdejesusmolina@gmail.com>
2023-09-10 15:00:18 -0600

    test: pass 4th gigasecond
    using method overloading

--- gigasecond/src/main/groovy/Gigasecond.groovy
+++ gigasecond/src/main/groovy/Gigasecond.groovy
@@ -4,7 +4,11 @@ import java.time.temporal.ChronoUnit
 
 class Gigasecond {
 
-    static def add(moment){
+    static def add(LocalDate moment){
         moment.atStartOfDay().plus(10**9, ChronoUnit.SECONDS)
+    }
+
+    static def add(LocalDateTime moment){
+        moment.plus(10**9, ChronoUnit.SECONDS)
     }
 }
